8775968f95c3690d6ab9287e801864deee23b2a2
vagrant <vagrant@vagrant.vm>
2017-08-10 21:52:39 +0000

    fixed bugs

--- README.md
+++ README.md
@@ -1,19 +1,61 @@
-## logs
+# logs
  
 A basic program that asnwers threee questions based on a provided data
 
 
 ## This project requires certain dependencies such as:
+
 PostgreSQL
 Psycopg2 API
+Python 
+Vagrant ("bento/ubuntu-16.04-i386")
+Virtual box
+
 
 
 ## To run it got to the log directory and run it:
-EG:
+The data was provided by my school and was manipulated in an virtual machine(vagarant) wih postgresql 
 
-CD/logs_analysis/
-python reporting.py
+After that I follow this steps.
+
+1.Import the data into the script with Psycopg2
+2.Create the below VIEWS
+3.Run the script(you know how to run a script. Right?)
 
 
 
-WARNING: Data not provided since it's to big :-(
+# Make sure you create the following VIEWS:
+
+
+## path_ok: counts the all paths where the status is '200 OK'
+ 
+            CREATE OR REPLACE VIEW path_ok AS select log.path, substring
+            (path from 10 for 30), COUNT(*) AS VIEWS FROM log WHERE status
+            LIKE '200 OK' GROUP BY  log.path; 
+
+
+## pop_articles: wich is combined with path ok giving us a total of all articles
+
+            CREATE OR REPLACE VIEW pop_articles AS SELECT views, substring,
+            slug, title,id, author FROM path_ok, articles WHERE slug =
+            path_ok.substring ORDER BY views DESC;
+
+
+## total_status: gets all the logs regardless its status code
+            
+            CREAT OR REPLACE VIEW total_status AS SELECT  
+            date(time) AS date, COUNT(status) AS count
+            FROM log  GROUP BY date;
+
+
+## err_status: gets logs with status(404)
+
+            CREATE OR REPLACE VIEW  err_status AS SELECT to_char
+            (time) AS date, COUNT(status) AS errors FROM log
+            WHERE status = '404 NOT FOUND' GROUP BY date;
+
+
+
+
+
+__WARNING: Data not provided since it's to big :-(__
--- reporting.py
+++ reporting.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+
 import psycopg2
 
 # Fetching tables from database and combine
@@ -11,7 +14,7 @@ c = db.cursor()
 # Create a view named "path_ok" counts the all paths where the status is
 # '200 OK'
 query = """ create or replace view path_ok as select log.path, substring
-            (path from 10 for 19), COUNT(*) AS VIEWS FROM log where status
+            (path from 10 for 30), COUNT(*) AS VIEWS FROM log where status
             like '200 OK' GROUP BY  log.path; """
 
 # create a view named "pop_articles" wich is combined with path_ok giving
@@ -20,13 +23,11 @@ query = """create or replace view pop_articles as select views, substring,
             slug, title,id, author from path_ok, articles where slug =
             path_ok.substring order by views desc;"""
 
-# Create a view called "pop_authors" wich is combined with pop articles
-query = """select name, sum(views)as views from pop_authors group by
-        name order by views desc;"""
+
 
 # Create a view "totals" total of dates with status
-query = """create or replace view total_status as select  to_char
-            (time, 'Month DD, YYYY') as date, count(status) as count
+query = """create or replace view total_status as select  
+            date(time) as date, count(status) as count
             from log  group by date;"""
 
 # Create a view with status errors
@@ -43,7 +44,7 @@ db.close()
 db = psycopg2.connect('dbname=news')
 c = db.cursor()
 
-query = "select title, views from pop_articles order  by views desc;"
+query = "select title, views from pop_articles order  by views desc limit 3;"
 
 
 c.execute(query)
@@ -58,11 +59,12 @@ print "The top articles are:"
 print
 for row in rows:
     print
-    print row[0], row[1], "- views"
+    print row[0], row[1], "-views"
 
 # Top authors
-query = """select name, sum(views)as views from pop_authors group by name
-            order by views desc;"""
+query = """ select authors.name, pop_articles.views from pop_articles,
+            authors where authors.id = pop_articles.author order by 
+            views desc limit 3;"""
 
 
 c.execute(query)
@@ -73,11 +75,11 @@ print "The top authors are:"
 print
 for row in rows:
     print
-    print row[0], row[1], "- views"
+    print row[0], row[1], "-views"
 
 
 # Divide Errros fromm "err_status" by count from total_status
-query = """select round((errors/count::float)*100) results, err_status.date
+query = """select round((errors/count::decimal)*100, 2) results, err_status.date
     from err_status, total_status where err_status. date= total_status.date;"""
 
 
@@ -86,11 +88,11 @@ rows = c.fetchall()
 print
 print
 print "============================="
-print "Errror beyond 1.5% on:"
+print "Errror beyond 1% on:"
 print
 
 for row in rows:
-    if row[0] > 1.5:
+    if row[0] > 1:
         print
         print row[1], "   ", row[0], "%"
 
