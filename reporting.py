8fd5e177205406f7753ab78882923ecd0e2b2808
vagrant <vagrant@vagrant.vm>
2017-08-10 16:50:22 +0000

    Pep8 and added txt file

--- reporting.py
+++ reporting.py
@@ -4,34 +4,35 @@ import psycopg2
 # ====Top three article fetch===
 
 
-
-
-    
 db = psycopg2.connect('dbname=news')
 c = db.cursor()
 
 
-#Create a view named "path_ok" counts the all paths where the status is '200 OK' 
+# Create a view named "path_ok" counts the all paths where the status is
+# '200 OK'
 query = """ create or replace view path_ok as select log.path, substring
-(path from 10 for 19), COUNT(*) AS VIEWS FROM log where status like '200 OK' 
-GROUP BY  log.path; """
+            (path from 10 for 19), COUNT(*) AS VIEWS FROM log where status
+            like '200 OK' GROUP BY  log.path; """
 
-#create a view named "pop_articles" wich is combined with path_ok giving us a total of all articles
-query = """create or replace view pop_articles as select views, substring, slug, title,id, author 
-from path_ok, articles where slug = path_ok.substring  order by views desc;""" 
+# create a view named "pop_articles" wich is combined with path_ok giving
+# us a total of all articles
+query = """create or replace view pop_articles as select views, substring,
+            slug, title,id, author from path_ok, articles where slug =
+            path_ok.substring order by views desc;"""
 
-#create a view called "pop_authors" wich is combined with pop articles
-query="""select name, sum(views)as views from pop_authors group by name order by views desc;"""
+# Create a view called "pop_authors" wich is combined with pop articles
+query = """select name, sum(views)as views from pop_authors group by
+        name order by views desc;"""
 
-#create a view "totals" total of dates with status
-query ="""create or replace view total_status as select  to_char(time, 'Month DD, YYYY') as date, 
-count(status) as count from log  group by date;"""
+# Create a view "totals" total of dates with status
+query = """create or replace view total_status as select  to_char
+            (time, 'Month DD, YYYY') as date, count(status) as count
+            from log  group by date;"""
 
-#CReate a view with status errors
-query = """CREATE OR REPLACE VIEW  err_status AS SELECT to_char(time, 'Month DD, YYYY') as date, 
-COUNT(status) as errors from log WHERE status = '404 NOT FOUND' GROUP BY date;"""
-   
-
+# Create a view with status errors
+query = """CREATE OR REPLACE VIEW  err_status AS SELECT to_char
+            (time, 'Month DD, YYYY') as date, COUNT(status) as errors from log
+            WHERE status = '404 NOT FOUND' GROUP BY date;"""
 
 c.execute(query)
 db.commit()
@@ -58,10 +59,10 @@ print
 for row in rows:
     print
     print row[0], row[1], "- views"
-    
 
-#top authors 
-query ="select name, sum(views)as views from pop_authors group by name order by views desc;"
+# Top authors
+query = """select name, sum(views)as views from pop_authors group by name
+            order by views desc;"""
 
 
 c.execute(query)
@@ -76,35 +77,22 @@ for row in rows:
 
 
 # Divide Errros fromm "err_status" by count from total_status
-query = "select round((errors/count::float)*100) results, err_status.date from err_status, total_status where err_status. date= total_status.date;"
+query = """select round((errors/count::float)*100) results, err_status.date
+    from err_status, total_status where err_status. date= total_status.date;"""
 
 
 c.execute(query)
 rows = c.fetchall()
 print
-print  
+print
 print "============================="
-print "Errror beyong 1.5% on:"
-print  
+print "Errror beyond 1.5% on:"
+print
 
 for row in rows:
     if row[0] > 1.5:
         print
-        print row[1], "   ", row[0] ,"%"
-    
-    
-    
-    
-    
+        print row[1], "   ", row[0], "%"
+
 db.commit()
 db.close()
-
-
-
-  
- 
-
-
-
-
-
